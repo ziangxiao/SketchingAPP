4f4ed7f46b78949d7d93e524a862778d28ca9176
RayneZhang <rayzhange@hotmail.com>
2017-08-30 23:43:53 -0500

    Add text hints.

---
+++ Assets/Scripts/UI/TextHint.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextHint : MonoBehaviour {
+
+    public GameObject ButtonInfo;
+
+    public void MouseEnter()
+    {
+        ButtonInfo.SetActive(true);
+    }
+
+    public void MouseExit()
+    {
+        ButtonInfo.SetActive(false);
+    }
+
+}
