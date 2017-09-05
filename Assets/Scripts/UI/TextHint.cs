bfa6e27d4c61e5f8ee39a2b1f86c7fa590de7f7d
RayneZhang <rayzhange@hotmail.com>
2017-09-05 17:22:28 +0800

    Changed Text Hint UI.

--- Assets/Scripts/UI/TextHint.cs
+++ Assets/Scripts/UI/TextHint.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class TextHint : MonoBehaviour {
 
     public GameObject ButtonInfo;
+    public GameObject TextPanel;
 
     public void MouseEnter()
     {
         ButtonInfo.SetActive(true);
+        TextPanel.SetActive(true);
     }
 
     public void MouseExit()
     {
         ButtonInfo.SetActive(false);
+        TextPanel.SetActive(false);
     }
 
 }
